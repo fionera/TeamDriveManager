f3ba153fda8f8481ced17c03dc5d95a6c729f65a
CatTheHacker <me@catthehacker.com>
2020-07-02 14:01:08 +0200

    add edit command to all.go

--- cmd/all/all.go
+++ cmd/all/all.go
@@ -4,6 +4,7 @@ import (
 	_ "github.com/fionera/TeamDriveManager/cmd/assign"
 	_ "github.com/fionera/TeamDriveManager/cmd/combinations"
 	_ "github.com/fionera/TeamDriveManager/cmd/create"
+	_ "github.com/fionera/TeamDriveManager/cmd/edit"
 	_ "github.com/fionera/TeamDriveManager/cmd/delete"
 	_ "github.com/fionera/TeamDriveManager/cmd/generate"
 	_ "github.com/fionera/TeamDriveManager/cmd/list"
